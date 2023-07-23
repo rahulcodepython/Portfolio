f7a62d96a5f3776250c1ad17269012082c52751e
rahulcodepython <rahulprofession01@gmail.com>
2023-07-24 03:02:54 +0530

    link error

--- src/app/freelance/components/client/breadcrumb.jsx
+++ src/app/freelance/components/client/breadcrumb.jsx
@@ -36,7 +36,7 @@ const Breadcrumb = () => {
             {
                 link ? link.map((item, index) => {
                     return <li key={index}>
-                        <Link href={item[1]} className="flex items-center">
+                        <Link href={item[1]} className="flex items-center" replace>
                             <BiSolidChevronRight />
                             <span className={`text-xs sm:text-sm font-semibold ${item[1] === '#' ? 'cursor-default' : 'hover:text-primary'}`}>
                                 {item[0]}
