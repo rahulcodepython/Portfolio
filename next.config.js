b980cde73b11482c49338e643a9bce3e5c87bfdc
Rahul Das <77882624+rahulcodepython@users.noreply.github.com>
2023-09-06 20:36:16 +0530

    export mode is added

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: false,
+    output: 'export',
     env: {
         MONGODB_URL: process.env.MONGODB_URL,
         DOMAIN_NAME: process.env.DOMAIN_NAME,
