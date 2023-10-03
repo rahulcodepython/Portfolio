796fdba85e1376b1a7228a68106fbee7cdbb4b97
Rahul Das <77882624+rahulcodepython@users.noreply.github.com>
2023-10-03 22:00:42 +0530

    Post Production Error fix

--- src/app/error.jsx
+++ src/app/error.jsx
@@ -9,11 +9,7 @@ export default function Error({ error, reset }) {
     return (
         <div>
             <h2>Something went wrong!</h2>
-            <button
-                onClick={
-                    () => reset()
-                }
-            >
+            <button onClick={() => reset()}>
                 Try again
             </button>
         </div>
