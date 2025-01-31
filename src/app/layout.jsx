beeb079e48c7f7165da2a4b1080d7589f7a787bc
rahulcodepython <rahulcodepython@gmail.com>
2025-01-31 07:26:05 +0530

    Add sitemap configuration and robots.txt for SEO optimization

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -2,19 +2,20 @@ import "./globals.css";
 import { Toaster } from "@/components/ui/sonner"
 
 export const metadata = {
-    title: 'Rahul Das | Full Stack Developer',
-    description: 'This is a personal portfolio website to describe me and also my experties and specialities even my services delivered by Rahul Das.',
+	title: 'Rahul Das | Full Stack Developer',
+	description: 'This is a personal portfolio website to describe me and also my experties and specialities even my services delivered by Rahul Das.',
+	keywords: ['Full Stack Developer', 'Next.js', 'React.js', 'Django', 'Portfolio', 'Rahul Das', 'Docker', 'rahulcodepython'],
 };
 
 export default function RootLayout({ children }) {
-    return (
-        <html lang="en" className="scroll-smooth">
-            <link rel="icon" href="/favicon.ico" sizes="any" />
-            <meta name='google-site-verification' content='r8h5jzR7gIadUgo08yu2g1rBugd24XFzS7Wa6P5tomw' />
-            <body className="bg-[url('/background.png')] bg-cover bg-fixed">
-                {children}
-                <Toaster />
-            </body>
-        </html>
-    );
+	return (
+		<html lang="en" className="scroll-smooth">
+			<link rel="icon" href="/favicon.ico" sizes="any" />
+			<meta name='google-site-verification' content='r8h5jzR7gIadUgo08yu2g1rBugd24XFzS7Wa6P5tomw' />
+			<body className="bg-[url('/background.png')] bg-cover bg-fixed">
+				{children}
+				<Toaster />
+			</body>
+		</html>
+	);
 }
