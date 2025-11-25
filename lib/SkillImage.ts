27917ed7617a9c1efb46c48fe6903616a8914fda
rahulcodepython <rahulcodepython@gmail.com>
2025-11-25 18:07:19 +0530

    sqlite3 is not supported by vercel so just for now this way is adapted.

--- lib/SkillImage.ts
+++
@@ -1,4 +0,0 @@
-export const SkillsImage = (skill: string): string => {
-	const skillID = skill.toLowerCase();
-	return `/tech-icon/${skillID}.svg`;
-}
---
+++ lib/data.ts
@@ -0,0 +1,397 @@
+export const getSettings: Settings = {
+    id: 1,
+    github_url: 'https://github.com/rahulcodepython',
+    linkedin_url: 'https://www.linkedin.com/in/rahulcodepython/',
+    twitter_url: 'https://twitter.com/rahulcodepython',
+    facebook_url: 'https://www.facebook.com/rahulcodepython',
+    instagram_url: 'https://www.instagram.com/rahulcodepython',
+    cv_url: null,
+    image_url: '/01.jpg',
+    updated_at: '2025-11-25 11:01:15'
+}
+
+export const getSkills: Skill[] = [
+    {
+        id: 1,
+        name: 'React.js',
+        category: 'frontend',
+        proficiency_level: 'Advanced',
+        created_at: '2025-11-25 11:01:15'
+    },
+    {
+        id: 2,
+        name: 'Next.js',
+        category: 'frontend',
+        proficiency_level: 'Advanced',
+        created_at: '2025-11-25 11:01:15'
+    },
+    {
+        id: 3,
+        name: 'TypeScript',
+        category: 'frontend',
+        proficiency_level: 'Intermediate',
+        created_at: '2025-11-25 11:01:15'
+    },
+    {
+        id: 4,
+        name: 'JavaScript',
+        category: 'frontend',
+        proficiency_level: 'Advanced',
+        created_at: '2025-11-25 11:01:15'
+    },
+    {
+        id: 5,
+        name: 'HTML5',
+        category: 'frontend',
+        proficiency_level: 'Advanced',
+        created_at: '2025-11-25 11:01:15'
+    },
+    {
+        id: 6,
+        name: 'CSS3',
+        category: 'frontend',
+        proficiency_level: 'Advanced',
+        created_at: '2025-11-25 11:01:15'
+    },
+    {
+        id: 7,
+        name: 'Tailwind CSS',
+        category: 'frontend',
+        proficiency_level: 'Advanced',
+        created_at: '2025-11-25 11:01:15'
+    },
+    {
+        id: 8,
+        name: 'Shadcn UI',
+        category: 'frontend',
+        proficiency_level: 'Intermediate',
+        created_at: '2025-11-25 11:01:15'
+    },
+    {
+        id: 9,
+        name: 'React Query',
+        category: 'frontend',
+        proficiency_level: 'Intermediate',
+        created_at: '2025-11-25 11:01:15'
+    },
+    {
+        id: 10,
+        name: 'Zustand',
+        category: 'frontend',
+        proficiency_level: 'Intermediate',
+        created_at: '2025-11-25 11:01:15'
+    },
+    {
+        id: 11,
+        name: 'React Native',
+        category: 'frontend',
+        proficiency_level: 'Intermediate',
+        created_at: '2025-11-25 11:01:15'
+    },
+    {
+        id: 12,
+        name: 'Django',
+        category: 'backend',
+        proficiency_level: 'Advanced',
+        created_at: '2025-11-25 11:01:15'
+    },
+    {
+        id: 13,
+        name: 'Django REST Framework',
+        category: 'backend',
+        proficiency_level: 'Advanced',
+        created_at: '2025-11-25 11:01:15'
+    },
+    {
+        id: 14,
+        name: 'Node.js',
+        category: 'backend',
+        proficiency_level: 'Intermediate',
+        created_at: '2025-11-25 11:01:15'
+    },
+    {
+        id: 15,
+        name: 'Express.js',
+        category: 'backend',
+        proficiency_level: 'Intermediate',
+        created_at: '2025-11-25 11:01:15'
+    },
+    {
+        id: 16,
+        name: 'Socket.io',
+        category: 'backend',
+        proficiency_level: 'Intermediate',
+        created_at: '2025-11-25 11:01:15'
+    },
+    {
+        id: 17,
+        name: 'Flask',
+        category: 'backend',
+        proficiency_level: 'Intermediate',
+        created_at: '2025-11-25 11:01:15'
+    },
+    {
+        id: 18,
+        name: 'Python',
+        category: 'backend',
+        proficiency_level: 'Advanced',
+        created_at: '2025-11-25 11:01:15'
+    },
+    {
+        id: 19,
+        name: 'REST APIs',
+        category: 'backend',
+        proficiency_level: 'Advanced',
+        created_at: '2025-11-25 11:01:15'
+    },
+    {
+        id: 20,
+        name: 'Golang',
+        category: 'backend',
+        proficiency_level: 'Intermediate',
+        created_at: '2025-11-25 11:01:15'
+    },
+    {
+        id: 21,
+        name: 'Fiber',
+        category: 'backend',
+        proficiency_level: 'Intermediate',
+        created_at: '2025-11-25 11:01:15'
+    },
+    {
+        id: 22,
+        name: 'PostgreSQL',
+        category: 'database',
+        proficiency_level: 'Intermediate',
+        created_at: '2025-11-25 11:01:15'
+    },
+    {
+        id: 23,
+        name: 'MongoDB',
+        category: 'database',
+        proficiency_level: 'Intermediate',
+        created_at: '2025-11-25 11:01:15'
+    },
+    {
+        id: 24,
+        name: 'Sqlite3',
+        category: 'database',
+        proficiency_level: 'Advanced',
+        created_at: '2025-11-25 11:01:15'
+    },
+    {
+        id: 25,
+        name: 'Prisma',
+        category: 'database',
+        proficiency_level: 'Intermediate',
+        created_at: '2025-11-25 11:01:15'
+    },
+    {
+        id: 26,
+        name: 'Raw SQL',
+        category: 'database',
+        proficiency_level: 'Intermediate',
+        created_at: '2025-11-25 11:01:15'
+    },
+    {
+        id: 27,
+        name: 'Docker',
+        category: 'devops',
+        proficiency_level: 'Intermediate',
+        created_at: '2025-11-25 11:01:15'
+    },
+    {
+        id: 28,
+        name: 'Git',
+        category: 'devops',
+        proficiency_level: 'Advanced',
+        created_at: '2025-11-25 11:01:15'
+    },
+    {
+        id: 29,
+        name: 'GitHub',
+        category: 'devops',
+        proficiency_level: 'Advanced',
+        created_at: '2025-11-25 11:01:15'
+    },
+    {
+        id: 30,
+        name: 'CI/CD',
+        category: 'devops',
+        proficiency_level: 'Intermediate',
+        created_at: '2025-11-25 11:01:15'
+    },
+    {
+        id: 31,
+        name: 'Linux',
+        category: 'devops',
+        proficiency_level: 'Intermediate',
+        created_at: '2025-11-25 11:01:15'
+    },
+    {
+        id: 32,
+        name: 'Nginx',
+        category: 'devops',
+        proficiency_level: 'Intermediate',
+        created_at: '2025-11-25 11:01:15'
+    },
+    {
+        id: 33,
+        name: 'Wordpress',
+        category: 'devops',
+        proficiency_level: 'Intermediate',
+        created_at: '2025-11-25 11:01:15'
+    },
+    {
+        id: 34,
+        name: 'ubuntu',
+        category: 'devops',
+        proficiency_level: 'Intermediate',
+        created_at: '2025-11-25 11:01:15'
+    },
+    {
+        id: 35,
+        name: 'Vercel',
+        category: 'deployment',
+        proficiency_level: 'Advanced',
+        created_at: '2025-11-25 11:01:15'
+    },
+    {
+        id: 36,
+        name: 'Netlify',
+        category: 'deployment',
+        proficiency_level: 'Intermediate',
+        created_at: '2025-11-25 11:01:15'
+    },
+    {
+        id: 37,
+        name: 'DigitalOcean',
+        category: 'deployment',
+        proficiency_level: 'Intermediate',
+        created_at: '2025-11-25 11:01:15'
+    },
+    {
+        id: 38,
+        name: 'PythonAnyWhere',
+        category: 'deployment',
+        proficiency_level: 'Intermediate',
+        created_at: '2025-11-25 11:01:15'
+    },
+    {
+        id: 39,
+        name: 'Railway',
+        category: 'deployment',
+        proficiency_level: 'Intermediate',
+        created_at: '2025-11-25 11:01:15'
+    },
+    {
+        id: 40,
+        name: 'VSCode',
+        category: 'others',
+        proficiency_level: 'Advanced',
+        created_at: '2025-11-25 11:01:15'
+    },
+    {
+        id: 41,
+        name: 'Neovim',
+        category: 'others',
+        proficiency_level: 'Intermediate',
+        created_at: '2025-11-25 11:01:15'
+    },
+    {
+        id: 42,
+        name: 'Postman',
+        category: 'others',
+        proficiency_level: 'Advanced',
+        created_at: '2025-11-25 11:01:15'
+    },
+    {
+        id: 43,
+        name: 'Mongodb Compass',
+        category: 'others',
+        proficiency_level: 'Intermediate',
+        created_at: '2025-11-25 11:01:15'
+    },
+    {
+        id: 44,
+        name: 'Figma',
+        category: 'others',
+        proficiency_level: 'Intermediate',
+        created_at: '2025-11-25 11:01:15'
+    },
+    {
+        id: 45,
+        name: 'Ereaser.io',
+        category: 'others',
+        proficiency_level: 'Intermediate',
+        created_at: '2025-11-25 11:01:15'
+    }
+]
+
+export const getProjects: Project[] = [
+    {
+        id: 1,
+        title: 'Libom',
+        description: 'An online library management system supporting book borrowing, returns, user authentication, and administrative controls with a sleek frontend and secure backend.',
+        technologies: 'FullStack, Next.js, Mongodb',
+        image_url: 'https://raw.githubusercontent.com/rahulcodepython/file-storage/main/portfolio/projectImage/ChatGPT%20Image%20Jun%2028%2C%202025%2C%2004_38_33%20PM.png',
+        live_url: 'https://libom.rahulcodepython.space/',
+        github_url: 'https://github.com/rahulcodepython/Libom.git',
+        featured: 1,
+        created_at: '2025-11-25 11:01:15'
+    },
+    {
+        id: 2,
+        title: 'CourseHunt',
+        description: 'A web platform to discover, filter, and track online courses across providers, with user profiles, ratings, and recommendation logic.',
+        technologies: 'FullStack, Next.js, Mongodb, Clean UI',
+        image_url: 'https://raw.githubusercontent.com/rahulcodepython/file-storage/main/portfolio/projectImage/Copilot_20250628_164424.png',
+        live_url: 'https://coursehunt.rahulcodepython.space/',
+        github_url: 'https://github.com/rahulcodepython/CourseHunt.git',
+        featured: 1,
+        created_at: '2025-11-25 11:01:15'
+    },
+    {
+        id: 5,
+        title: 'Multiplayer Chess',
+        description: 'A real-time chess application enabling live multiplayer matches, move validation, and in-game chat using websockets and persistent backend logic.',
+        technologies: 'Express.js, React.js, Socket.io, Chess.js',
+        image_url: 'https://raw.githubusercontent.com/rahulcodepython/file-storage/main/portfolio/projectImage/Copilot_20250628_171806.png',
+        live_url: '',
+        github_url: 'https://github.com/rahulcodepython/Multiplayer-Chess-Frontend.git',
+        featured: 1,
+        created_at: '2025-11-25 11:01:15'
+    }
+]
+
+export type Settings = {
+    id: number;
+    github_url: string;
+    linkedin_url: string;
+    twitter_url: string;
+    facebook_url: string;
+    instagram_url: string;
+    cv_url: string | null;
+    image_url: string;
+    updated_at: string;
+};
+
+export type Skill = {
+    id: number;
+    name: string;
+    category: 'frontend' | 'backend' | 'database' | 'devops' | 'deployment' | 'others';
+    proficiency_level: 'Advanced' | 'Intermediate';
+    created_at: string;
+};
+
+export type Project = {
+    id: number;
+    title: string;
+    description: string;
+    technologies: string;
+    image_url: string;
+    live_url: string;
+    github_url: string;
+    featured: number;
+    created_at: string;
+};
